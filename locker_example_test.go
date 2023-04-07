cdc3e2f952de683aaeb1bd382734476dbee6649c
Bulat Khasanov <afti@yandex.ru>
2023-04-07 08:19:24 +0300

    Update README

--- locker_example_test.go
+++ locker_example_test.go
@@ -1,10 +1,4 @@
-# redislock
-Redis context-based locker with auto refresh.
-
-## Examples
-
-```go
-package main
+package redislock_test
 
 import (
 	"context"
@@ -14,22 +8,19 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func doJobUnderLockWithContext(ctx context.Context) {
+func doJobUnderLockWithContext(ctx context.Context) { //nolint:revive
 }
 
-func main() {
+func ExampleLocker_Lock() {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
 
-	// Lock will automatically update the TTL time every RefreshPeriod.
-	// The configuration can be set either for the entire Locker or for each Lock request individually.
 	locker, err := redislock.New(rdb, redislock.WithRefreshPeriod(time.Minute), redislock.WithTTL(10*time.Minute))
 	if err != nil {
 		panic(err)
 	}
 
-	// Try to get lock.
 	lock, ctx, err := locker.Lock(context.Background(), "my_lock_key")
 	if err != nil {
 		panic(err)
@@ -38,7 +29,6 @@ func main() {
 	defer func() {
 		_ = lock.Unlock()
 	}()
-	
+
 	doJobUnderLockWithContext(ctx)
 }
-```
